bacb1a94012a0e0782ceb765d7c8bca11a739b3f
gocronx <gocronx@gmail.com>
2026-03-30 16:51:26 +0800

    feat: update commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -34,12 +34,11 @@ module.exports = {
     'type-empty': [0], // Allow empty type
     'subject-empty': [0] // Allow empty subject
   },
-  // Custom parser preset, support emoji
+  // Standard conventional commit parser
   parserPreset: {
     parserOpts: {
-      headerPattern:
-        /^(?:([\u00a9\u00ae\u2000-\u3300\ud83c\ud000-\udfff\ud83d\ud000-\udfff\ud83e\ud000-\udfff])\s)?([\w-]+)(?:\(([\w-]+)\))?:\s(.+)$/,
-      headerCorrespondence: ['emoji', 'type', 'scope', 'subject']
+      headerPattern: /^([\w-]+)(?:\(([\w-]+)\))?:\s(.+)$/,
+      headerCorrespondence: ['type', 'scope', 'subject']
     }
   },
 
@@ -58,18 +57,18 @@ module.exports = {
     },
     // prettier-ignore
     types: [
-      { value: "✨ feat",     name: "feat:     ✨ A new feature", emoji: "✨" },
-      { value: "🐛 fix",      name: "fix:      🐛 A bug fix", emoji: "🐛" },
-      { value: "📝 docs",     name: "docs:     📝 Documentation only changes", emoji: "📝" },
-      { value: "💄 style",    name: "style:    💄 Changes that do not affect the meaning of the code", emoji: "💄" },
-      { value: "♻️ refactor", name: "refactor: ♻️ A code change that neither fixes a bug nor adds a feature", emoji: "♻️" },
-      { value: "⚡️ perf",     name: "perf:     ⚡️ A code change that improves performance", emoji: "⚡️" },
-      { value: "✅ test",     name: "test:     ✅ Adding missing tests or correcting existing tests", emoji: "✅" },
-      { value: "📦️ build",    name: "build:    📦️ Changes that affect the build system or external dependencies", emoji: "📦️" },
-      { value: "🎡 ci",       name: "ci:       🎡 Changes to our CI configuration files and scripts", emoji: "🎡" },
-      { value: "⏪️ revert",   name: "revert:   ⏪️ Reverts a previous commit", emoji: "⏪️" },
-      { value: "🔨 chore",    name: "chore:    🔨 Other changes that don't modify src or test files", emoji: "🔨" },
-      { value: "🚧 wip",      name: "wip:      🚧 Work in progress", emoji: "🚧" },
+      { value: "feat",     name: "feat:     A new feature" },
+      { value: "fix",      name: "fix:      A bug fix" },
+      { value: "docs",     name: "docs:     Documentation only changes" },
+      { value: "style",    name: "style:    Changes that do not affect the meaning of the code" },
+      { value: "refactor", name: "refactor: A code change that neither fixes a bug nor adds a feature" },
+      { value: "perf",     name: "perf:     A code change that improves performance" },
+      { value: "test",     name: "test:     Adding missing tests or correcting existing tests" },
+      { value: "build",    name: "build:    Changes that affect the build system or external dependencies" },
+      { value: "ci",       name: "ci:       Changes to our CI configuration files and scripts" },
+      { value: "revert",   name: "revert:   Reverts a previous commit" },
+      { value: "chore",    name: "chore:    Other changes that don't modify src or test files" },
+      { value: "wip",      name: "wip:      Work in progress" },
     ],
     useEmoji: false,
     emojiAlign: 'center',
